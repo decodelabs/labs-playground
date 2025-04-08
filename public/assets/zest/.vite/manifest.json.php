d091aff007479765b7b0db97a36de62303af2c3a
Tom Wright <tom@inflatablecookie.com>
2025-04-08 08:11:06 +0000

    Updated vite config to ts
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- public/assets/zest/.vite/manifest.json.php
+++ public/assets/zest/.vite/manifest.json.php
@@ -7,7 +7,7 @@ use DecodeLabs\Zest\Manifest;
 /* Auto-generated Zest manifest cache file */
 return (new Manifest(__DIR__ . '/manifest.json', false))
     ->addBodyJs([
-        '/assets/zest/main-DYUZFy11.js' => [
+        '/assets/zest/main-BbBhivc3.js' => [
             'type' => 'module',
         ],
     ])
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import castaway from '@decodelabs/vite-plugin-castaway';
+import zest from '@decodelabs/vite-plugin-zest';
+import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        castaway(),
+        zest({
+            buildOnExit: true,
+            mergeToPublicDir: true,
+            publicCacheBuster: true
+        }),
+        //react(),
+        vue(),
+    ],
+
+    resolve: {
+        alias: {
+            '@components': './src/@components',
+            '@scripts': './src/@scripts',
+            '@styles': './src/@styles',
+        },
+    },
+
+    build: {
+        rollupOptions: {
+            input: 'src/@scripts/main.js'
+        }
+    },
+
+    server: {
+        port: 6543,
+        cors: {
+            origin: '*'
+        }
+    }
+})
