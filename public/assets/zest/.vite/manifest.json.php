03e38ad1103adfb3376ffde7f7cac8d23db02bf6
Tom Wright <tom@inflatablecookie.com>
2025-04-30 18:24:16 +0000

    Switched to pnpm
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- public/assets/zest/.vite/manifest.json.php
+++ public/assets/zest/.vite/manifest.json.php
@@ -7,7 +7,7 @@ use DecodeLabs\Zest\Manifest;
 /* Auto-generated Zest manifest cache file */
 return (new Manifest(__DIR__ . '/manifest.json', false))
     ->addBodyJs([
-        '/assets/zest/main-BbBhivc3.js' => [
+        '/assets/zest/main-C4Ur0_xd.js' => [
             'type' => 'module',
         ],
     ])
