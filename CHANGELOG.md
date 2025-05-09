22756377bf8a93b8a35ca28e9db529b35277105d
Tom Wright <tom@inflatablecookie.com>
2025-05-09 11:22:55 +0000

    Upgraded Harvest to v0.5.0
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
-## v0.1.0 (2025-03-03)
+## v0.1.0 (2025-?-?)
 * Built initial implementation
+* Converted Actions to Pages
+* Upgraded Harvest to v0.5.0
--- src/App.php
+++ src/App.php
@@ -10,32 +10,10 @@ declare(strict_types=1);
 namespace DecodeLabs\Playground;
 
 use DecodeLabs\Fabric\App\Generic;
-use DecodeLabs\Genesis;
 use DecodeLabs\Monarch;
 
 class App extends Generic
 {
-    /*
-    public function prepareHttpMiddleware(): ?array
-    {
-        return [
-            // Error
-            'ErrorHandler',
-
-            // Inbound
-            'Https',
-            'Cors',
-
-            // Outbound
-            'ContentSecurityPolicy',
-
-            // Generators
-            'Zest',
-            'Greenleaf'
-        ];
-    }
-        */
-
     public function initializePlatform(): void
     {
         Monarch::$paths->alias('@public', '@run/public');
