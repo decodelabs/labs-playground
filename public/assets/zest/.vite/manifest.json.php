d4e94a1dadfe3b20966f2a2e8d422d0d2c9c65a9
Tom Wright <tom@inflatablecookie.com>
2025-09-16 11:27:39 +0100

    Updated packages
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- public/assets/zest/.vite/manifest.json.php
+++ public/assets/zest/.vite/manifest.json.php
@@ -7,7 +7,7 @@ use DecodeLabs\Zest\Manifest;
 /* Auto-generated Zest manifest cache file */
 return (new Manifest(__DIR__ . '/manifest.json', false))
     ->addBodyJs([
-        '/assets/zest/main-C4Ur0_xd.js' => [
+        '/assets/zest/main-DL37Yh0G.js' => [
             'type' => 'module',
         ],
     ])
